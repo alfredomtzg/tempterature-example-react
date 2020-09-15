6d1b5381d99ae686c336549b1d04ade1dde847de
Alfredo Martinez <psialfredomtz@gmail.com>
2020-09-15 01:08:16 -0500

    change to function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
+
+  const[temperatureValue, setTemperatureValue] = useState(10);
+  const[temperatureColor, setTemperatureColor] = useState("cold");
+
+  const increaseTemperature = () => {
+    setTemperatureValue( temperatureValue + 1 )
+  }
+
   return(
-    <div>Hola soy una function</div>
+    <div className="app-container" >
+      <div className="temperature-display-container" >
+        <div className={`temperature-display ${temperatureColor}`} > {temperatureValue}°C</div>
+      </div>
+      <div className="button-container" >
+        <button onClick={() => increaseTemperature()} >+</button>
+        <button onClick={() => setTemperatureValue(temperatureValue-1)}>-</button>
+      </div>
+    </div>
   )
 }
